ab1352fed736934b8c41b6a143becde7192ba7de
Dewald de Jager <dewald.dj@hotmail.com>
2020-04-17 19:11:36 +0200

    Add support for the latest version of Wiremock (2.26.3)

--- src/main/java/com/github/masonm/JwtStubMappingTransformer.java
+++ src/main/java/com/github/masonm/JwtStubMappingTransformer.java
@@ -79,6 +79,7 @@ public class JwtStubMappingTransformer extends StubMappingTransformer {
             outer.getBasicAuthCredentials(),
             outer.getBodyPatterns(),
             null,
+            null,
             outer.getMultipartPatterns()
         );
     }
